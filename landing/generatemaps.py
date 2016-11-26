aef8e873aebfed9bf8b1bd4716d244608fc1748d
Marcela Rosales <marcelarosalesj@gmail.com>
2016-11-26 14:33:05 -0600

    Add feature on generatemaps.py to display the 3d map generated. Also modified the graph generated
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- landing/generatemaps.py
+++ landing/generatemaps.py
@@ -5,13 +5,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def generateSin():
-	X = np.arange(0, 200,1.0)
-	Y = np.arange(0, 200,1.0)
+	X = np.arange(0, 199, 1.0)
+	Y = np.arange(0, 199, 1.0)
 	X, Y = np.meshgrid(X,Y)
-	R = np.sqrt(X**2 + Y**2)
+	R = np.sqrt( (X+3))
+	super_threshold_indices = R < -1
+	R[super_threshold_indices] = 0
 	Z = abs(np.sin(R))
 	np.savetxt("./maps/sin.txt", Z, fmt="%.4f", delimiter="\t", newline="\n")
-	
+
+	fig = plt.figure()
+	ax = fig.gca(projection='3d')
+
+	surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+	ax.set_zlim(-1.01, 1.01)
+
+	ax.zaxis.set_major_locator(LinearLocator(10))
+	ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+	fig.colorbar(surf, shrink=0.5, aspect=5)
+
+	plt.show()
 	
 
 
